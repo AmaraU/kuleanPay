add374602081e8890204250dd773685900d37f27
AmaraU <apu24@drexel.edu>
2024-05-09 14:20:12 +0100

    Update Escrow2.jsx

--- kulean-pay/src/Components/Escrow2/Escrow2.jsx
+++ kulean-pay/src/Components/Escrow2/Escrow2.jsx
@@ -1,39 +1,45 @@
 import React from "react";
-
+import styles from "./Escrow.module.css";
 import { getImageUrl } from "../../utils";
-import styles from "./Escrow2.module.css"
 
-export const Escrow2 = () => {
-    return <div className={styles.Escrow}>
-        <div className={styles.img}>
-            <img src={getImageUrl("frame4.png")} alt="e.s" />
+export const Escrow = () => {
+    return (
+        <>
+        <div className={styles.whole}>
+            <div className={styles.text}>
+                <h1>Buy & sell with Kuleanpay Escrow</h1>
+                <div className={styles.items}>
+                    <div className={styles.item}>
+                        <div className={styles.icon}><img src={getImageUrl("icons/escowIcon1.png")} alt="" /></div>
+                        <div className={styles.itemText}>
+                            <h4>Secure Transactions</h4>
+                            <p>Seamless, transparent and secure transactions.</p>
+                        </div>
+                    </div>
+                    
+                    <div className={styles.item}>
+                        <div className={styles.icon}><img src={getImageUrl("icons/escowIcon2.png")} alt="" /></div>
+                        <div className={styles.itemText}>
+                            <h4>Quality Service</h4>
+                            <p>Authorize or receive payments seamlessly.</p>
+                        </div>
+                    </div>
+
+                    <div className={styles.item}>
+                        <div className={styles.icon}><img src={getImageUrl("icons/escowIcon3.png")} alt="" /></div>
+                        <div className={styles.itemText}>
+                            <h4>Timely Delivery</h4>
+                            <p>The buyer and seller can track packages in real time.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className={styles.image}>
+                <img className={styles.img1} src={getImageUrl("escowImg.png")} alt="" />
+                <img className={styles.img2} src={getImageUrl("escrowImg2.png")} alt="" />
+            </div>
         </div>
-        <div className={styles.text}>
-            <div className={styles.header}>
-                <h1>Buy & sell with Kuleanpay Escrow </h1>
-            </div>
-            <div className={styles.bread}>
-                <img src={getImageUrl("pic1.png")}alt="pic" />
-                <div>
-                    <h4>Secure Transactions</h4>
-                    <p>Seamless,transparent and secure transactions</p>
-                </div>
-                
-            </div>
-            <div className={styles.crumbs}>
-                <img src={getImageUrl("pic2.png")} alt="p.s" />
-                <div>
-                    <h4>Quality Service</h4>
-                    <p>Authorize or receive payments seamlessly</p>
-                </div>
-            </div>
-            <div className={styles.alt}>
-                <img src={getImageUrl("pic3.png")} alt="p.p" />
-                <div>
-                    <h4>Timely Delivery</h4>
-                    <p>The buyer and seller can track packages in real time</p>
-                </div>
-            </div>
-        </div>
-    </div>
+        </>
+    )
 }
