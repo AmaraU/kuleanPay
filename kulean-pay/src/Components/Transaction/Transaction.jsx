5dd1b3bcb8721e14913a5b61478d6f1383c359c0
AmaraU <apu24@drexel.edu>
2024-05-11 00:57:30 +0100

    Last last

--- kulean-pay/src/Components/Transaction/Transaction.jsx
+++ kulean-pay/src/Components/Transaction/Transaction.jsx
@@ -8,7 +8,7 @@ export const Transaction = () => {
     <div className={styles.Transaction}>
         <div className={styles.text}>
             <div className={styles.heading}>
-                <h1>We protect both partieson all transactions</h1>
+                <h1>We protect both parties on all transactions</h1>
                 <p>We ensure that both buyers and sellers are protected on every transaction</p>
             </div>
             <div className={styles.button}>
